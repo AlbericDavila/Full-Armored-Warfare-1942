ac7fceea41c63778779808376b06e9bc2f7e40a7
Alberic A. Davila Cadilla <alberic.davila@gmail.com>
2017-09-08 21:45:25 -0400

    Trying to solve .dll problem

---
+++ FullArmoredWarfare/Intermediate/Build/Win64/UE4Editor/Inc/FullArmoredWarfare/FullArmoredWarfareGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FULLARMOREDWARFARE_FullArmoredWarfareGameModeBase_generated_h
+#error "FullArmoredWarfareGameModeBase.generated.h already included, missing '#pragma once' in FullArmoredWarfareGameModeBase.h"
+#endif
+#define FULLARMOREDWARFARE_FullArmoredWarfareGameModeBase_generated_h
+
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_RPC_WRAPPERS
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFullArmoredWarfareGameModeBase(); \
+	friend FULLARMOREDWARFARE_API class UClass* Z_Construct_UClass_AFullArmoredWarfareGameModeBase(); \
+public: \
+	DECLARE_CLASS(AFullArmoredWarfareGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/FullArmoredWarfare"), NO_API) \
+	DECLARE_SERIALIZER(AFullArmoredWarfareGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAFullArmoredWarfareGameModeBase(); \
+	friend FULLARMOREDWARFARE_API class UClass* Z_Construct_UClass_AFullArmoredWarfareGameModeBase(); \
+public: \
+	DECLARE_CLASS(AFullArmoredWarfareGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/FullArmoredWarfare"), NO_API) \
+	DECLARE_SERIALIZER(AFullArmoredWarfareGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFullArmoredWarfareGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFullArmoredWarfareGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFullArmoredWarfareGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFullArmoredWarfareGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFullArmoredWarfareGameModeBase(AFullArmoredWarfareGameModeBase&&); \
+	NO_API AFullArmoredWarfareGameModeBase(const AFullArmoredWarfareGameModeBase&); \
+public:
+
+
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFullArmoredWarfareGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFullArmoredWarfareGameModeBase(AFullArmoredWarfareGameModeBase&&); \
+	NO_API AFullArmoredWarfareGameModeBase(const AFullArmoredWarfareGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFullArmoredWarfareGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFullArmoredWarfareGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFullArmoredWarfareGameModeBase)
+
+
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_12_PROLOG
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_RPC_WRAPPERS \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_INCLASS \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FullArmoredWarfare_Source_FullArmoredWarfare_FullArmoredWarfareGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
